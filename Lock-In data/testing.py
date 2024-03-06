3d7389436a4a70ee8929687cf6a00779abd647da
matsr22 <62033711+matsr22@users.noreply.github.com>
2024-03-06 11:03:06 +0000

    Finalising Graphs

--- Lock-In data/testing.py
+++ Lock-In data/testing.py
@@ -1,6 +1,15 @@
 print(2 in range(2,0))
 import time
 print(int(time.time()))
+import matplotlib
 from matplotlib import pyplot as plt 
 
-print(plt.rcParams['lines.markersize'] **2)
+import numpy as np
+
+
+print(np.linspace(0,10,11))
+
+print(r"\pm")
+matplotlib.use('Agg')
+plt.plot([0,1,2,3],[0,1,2,3])
+plt.savefig(r"C:\Users\matth\Documents\Phys 382\Lock-In data\Finalised Plots\testimage2.png",metadata = {"Description":"This is a test","Title":"Exact Title"},format = "png")
