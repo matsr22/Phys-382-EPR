1b9855c9ea0a7f472c61b0e3df43de158239b7fb
Matt <matthewseb.rose@gmail.com>
2024-03-06 12:59:51 +0000

    Colour Plotting

--- Lock-In data/testing.py
+++ Lock-In data/testing.py
@@ -1,15 +1,22 @@
 print(2 in range(2,0))
 import time
 print(int(time.time()))
-import matplotlib
-from matplotlib import pyplot as plt 
+#import matplotlib
+#from matplotlib import pyplot as plt 
 
-import numpy as np
+#import numpy as np
 
 
-print(np.linspace(0,10,11))
+#print(np.linspace(0,10,11))
 
-print(r"\pm")
-matplotlib.use('Agg')
-plt.plot([0,1,2,3],[0,1,2,3])
-plt.savefig(r"C:\Users\matth\Documents\Phys 382\Lock-In data\Finalised Plots\testimage2.png",metadata = {"Description":"This is a test","Title":"Exact Title"},format = "png")
+#print(r"\pm")
+#matplotlib.use('Agg')
+#plt.plot([0,1,2,3],[0,1,2,3])
+#plt.savefig(r"C:\Users\matth\Documents\Phys 382\Lock-In data\Finalised Plots\testimage2.png",metadata = {"Description":"This is a test","Title":"Exact Title"},format = "png")
+
+array1 = [0,1,2,3,4]
+array2 = [5,6,7,8,9]
+
+for element1,element2 in array1,array2:
+    print(element1)
+    print(element2)
